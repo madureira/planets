7b051840e4cb7c321af24519ef361241e515d346
Rafael Madureira <rafael.madureira@wildlifestudios.com>
2021-04-24 14:46:14 -0300

    Add Layered Noise

---
+++ Assets/Scripts/NoiseSettings.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[System.Serializable]
+public class NoiseSettings
+{
+    public float strenght = 1;
+    [Range(1, 8)] public int numLayers = 1;
+    public float baseRoughness = 1;
+    public float roughness = 2;
+    public float persistence = 0.5f;
+    public Vector3 centre;
+    public float minValue;
+}
